6454129d10d1a91778bcf39e38dfa78a77e97bba
Nicolas Wolf <ndwolf@gmail.com>
2020-01-07 15:35:35 -0300

    added blockscout explorer

--- create-env.sh
+++ create-env.sh
@@ -13,10 +13,11 @@ if [ ! -e "./.env" ]; then
     export SECURE_WALLET_OAUTH2_CLIENT_SECRET=$(export LC_CTYPE=C; cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
     export SECURE_GREENHOUSE_SESSION_SECRET=$(export LC_CTYPE=C; cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
     export SECURE_GREENHOUSE_OAUTH2_CLIENT_SECRET=$(export LC_CTYPE=C; cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+    export SECURE_BLOCKSCOUT_SECRETS_SYSTEM=$(export LC_CTYPE=C; cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
     sed -e "s/secure_redis_password/${SECURE_REDIS_PASSWORD}/g" \
         -e "s/secure_mongo_password/${SECURE_MONGO_PASSWORD}/g" \
         -e "s/secure_postgres_password/${SECURE_POSTGRES_PASSWORD}/g" \
-        -e "s/secure_hydra_secrets_system/${SECURE_HYDRA_SECRETS_SYSTEM}/g" \
+        -e "s/secure_hydra_secrets_system/${SECURE_HYDRA_SECRETS_SYSTEM}/g" \        
         -e "s/secure_accounts_session_secret/${SECURE_ACCOUNTS_SESSION_SECRET}/g" \
         -e "s/secure_accounts_api_key/${SECURE_ACCOUNTS_API_KEY}/g" \
         -e "s/secure_accounts_oauth2_client_secret/${SECURE_ACCOUNTS_OAUTH2_CLIENT_SECRET}/g" \
@@ -25,6 +26,7 @@ if [ ! -e "./.env" ]; then
         -e "s/secure_wallet_oauth2_client_secret/${SECURE_WALLET_OAUTH2_CLIENT_SECRET}/g" \
         -e "s/secure_greenhouse_session_secret/${SECURE_GREENHOUSE_SESSION_SECRET}/g" \
         -e "s/secure_greenhouse_oauth2_client_secret/${SECURE_GREENHOUSE_OAUTH2_CLIENT_SECRET}/g" \
+        -e "s/secure_blockscout_secrets_system/${SECURE_BLOCKSCOUT_SECRETS_SYSTEM}/g" \
         ./.env.example > ./.env
     echo "File .env has been created"
 else
